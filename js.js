ce7aa112e87ca1ff34b36f8aa8f5cd840ad7bc20
kaldrma <109808649+kaldrma@users.noreply.github.com>
2022-09-17 17:00:25 +0200

    test za fetch

--- js.js
+++ js.js
@@ -43,3 +43,25 @@ const observer = new IntersectionObserver( entries => {
 elementi.forEach(element => {
     observer.observe(element)
 })
+
+//fetch zavrzlame
+
+function readJson () {
+    // http://localhost:8080
+    fetch('https://kaldrma.github.io/base.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        this.users = json;
+        console.log(this.users);
+    })
+    .catch(function () {
+        this.dataError = true;
+    })
+ }
+
+readJson()
